ddef64d6bda5cdec553c577f1d3df26b9313c607
tomtomy8 <tommy.birt@gmail.com>
2015-07-28 10:40:11 +0100

    mugger can move forward and back but floats up randomly in scene test

---
+++ Assets/Scripts/Battle.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var brawlers : Brawler[];
+
+function Start () {
+	
+}
+
+function Update () {
+		
+}
+
+function CalDamageTaken(attack : float, modifier : float, multiplier : float) {
+	return (attack * multiplier) + modifier;
+}
---
+++ Assets/Scripts/Brawler.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+var name : String;
+var isPlayer : boolean;
+var isBoss : boolean;
+var difficulty : int;
+var attack : float;
+var defense : float;
+var speed : float;
+var health : int;
+
+private var myAnimator : Animator;
+
+private var direction : String;
+
+function Start () {
+	myAnimator = gameObject.GetComponent("Animator");
+}
+
+function FixedUpdate () {
+	Move();
+	if(Input.GetKeyDown("d")) {
+		direction = "right";
+	} else if(Input.GetKeyDown("a")) {
+		direction = "left"; 
+	} else if (!Input.anyKey){
+		direction = null;
+	} 
+}
+
+function Move () {
+	
+	if(direction == "right") {
+		myAnimator.SetFloat("Speed", 1);
+		myAnimator.SetBool("IsFacingRight", true);
+		transform.Translate(Time.deltaTime, 0, 0);		
+	} else if (direction == "left") {
+		myAnimator.SetFloat("Speed", -1);
+		transform.Translate(-(Time.deltaTime), 0, 0);
+		myAnimator.SetBool("IsFacingRight", false);
+	} else {
+		transform.Translate(0,0,0);
+		myAnimator.SetFloat("Speed", 0);
+	}
+}
